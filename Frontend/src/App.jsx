53f07fbe597a0bb19e8b1feaef14ff78e0f47494
latoure324 <tukamothupi@gmail.com>
2026-05-20 13:41:48 +0200

    Initializing the capstone project

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+import Router from "./Router.jsx"
+import { GeneralStyle } from "./styles/General.styled.js"
+function App() {
+
+  return (
+  <GeneralStyle>
+    <Router/>
+  </GeneralStyle>
+  )
+}
+
+export default App
---
+++ Frontend/src/styles/General.styled.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const GeneralStyle = styled.div`
+    * {
+        background-color: transparent;
+        
+}
+    body {
+        padding: 0;
+        margin: 0;
+    }
+`
